72896a039a54a526d406fa11c631f5ea64a88d09
Oliver Forral <oforral@mesd.k12.or.us>
2015-06-23 09:21:00 -0700

    Added addRow function

--- Resources/public/js/filter.js
+++ Resources/public/js/filter.js
@@ -18,21 +18,7 @@ $( document ).ready(function()
                 html += '<th>' + msg[i] + '</th>'
             }
             html += '</tr>';
-            html += '<tr>';
-            for (var i = 0; i < msg.length; i++) {
-                if (0 < i) {
-                    html += '<td>AND</td>';
-                }
-                html += '<td>';
-                html += '<div>';
-                html += 'No ' + msg[i] + ' selected';
-                html += '</div>';
-                html += '<div>';
-                html += '<a class="btn btn-default" href="#">Edit Cell</a>';
-                html += '</div>';
-                html +='</td>';
-            }
-            html += '</tr>';
+            html += addRow(msg);
             html += '</table>';
             if (1 < msg.length) {
                 html += '<a id="filter-row-add" class="btn btn-default" href="#">Add Row</a>';
@@ -40,6 +26,38 @@ $( document ).ready(function()
             $('#filter-interface').html(
                 html
             );
+            $('#filter-row-add').on('click', function () {
+                var html = '';
+                html += '<tr>'
+                html += '<td colspan="' + ((msg.length * 2) - 1) + '">OR</td>'
+                html += '</tr>'
+                html += addRow(msg)
+                $('#filter-interface-table').append(
+                    html
+                );
+            });
         });
     });
+    
+    function addRow(msg)
+    {
+        var html = '';
+        html += '<tr>';
+        for (var i = 0; i < msg.length; i++) {
+            if (0 < i) {
+                html += '<td>AND</td>';
+            }
+            html += '<td>';
+            html += '<div>';
+            html += 'No ' + msg[i] + ' selected';
+            html += '</div>';
+            html += '<div>';
+            html += '<a class="btn btn-default" href="#">Edit Cell</a>';
+            html += '</div>';
+            html +='</td>';
+        }
+        html += '</tr>';
+
+        return html;
+    }
 });
