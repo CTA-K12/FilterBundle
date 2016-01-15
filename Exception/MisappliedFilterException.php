799b8610bc4249e8f1e2441d7d4ac4d1165600f4
Oliver Forral <oforral@mesd.k12.or.us>
2016-01-15 09:44:42 -0800

    fixing missing use statement issue in Insight

--- Exception/MisappliedFilterException.php
+++ Exception/MisappliedFilterException.php
@@ -8,7 +8,7 @@ namespace Mesd\FilterBundle\Exception;
 class MisappliedFilterException extends \Exception
 {
     // Redefine the exception so message isn't optional
-    public function __construct($message, $code = 0, Exception $previous = null) {
+    public function __construct($message, $code = 0, \Exception $previous = null) {
         // some code
     
         // make sure everything is assigned properly
